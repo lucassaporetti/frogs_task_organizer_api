1e702780da2e459dd58598d18b24af11c39e523a
Lucas Saporetti <lucassaporetti@gmail.com>
2020-10-07 17:05:37 -0300

    api created

--- api/api/urls.py
+++ api/api/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
+from core.views import TaskViewSet
+
+router = routers.DefaultRouter()
+router.register(r'tasks', TaskViewSet)
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls'))
 ]
--- api/core/models.py
+++ api/core/models.py
@@ -1,4 +1,14 @@
 from django.db import models
 
-class Task(models.Model):
 
+class Task(models.Model):
+    id = models.AutoField(primary_key=True)
+    status = models.CharField(max_length=50)
+    name = models.CharField(max_length=500)
+    date = models.CharField(max_length=50)
+    time = models.CharField(max_length=50)
+    type = models.CharField(max_length=50)
+    priority = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
