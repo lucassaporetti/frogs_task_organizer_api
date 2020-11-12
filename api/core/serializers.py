57b277a9520228b6c2b30060e8d3d2a8020eff09
Lucas Saporetti <lucassaporetti@gmail.com>
2020-11-12 18:57:13 -0300

    now put and delete methods are available

--- api/core/serializers.py
+++ api/core/serializers.py
@@ -1,8 +1,9 @@
-from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer
 from .models import Task
 
 
-class TaskSerializer(serializers.ModelSerializer):
+class TaskSerializer(ModelSerializer):
+
     class Meta:
         model = Task
         fields = ['uuid', 'status', 'name', 'date', 'time', 'task_type', 'priority']
--- api/core/views.py
+++ api/core/views.py
@@ -1,9 +1,15 @@
 from rest_framework import viewsets
+from rest_framework.generics import RetrieveUpdateDestroyAPIView
 from .models import Task
 from .serializers import TaskSerializer
 
 
-class TaskViewSet(viewsets.ModelViewSet):
+class TaskViewSet(viewsets.ModelViewSet, RetrieveUpdateDestroyAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+
+    def get_object(self):
+        return Task.objects.get()
+
+
 # Create your views here.
